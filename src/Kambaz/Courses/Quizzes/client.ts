ca4d6aeab0995e98a3667486bc23a9c24ac22f7f
Afnan Tuffaha <tuffaha.af@northeastern.edu>
2025-06-21 18:11:33 -0400

    find quiz by id

--- src/Kambaz/Courses/Quizzes/client.ts
+++ src/Kambaz/Courses/Quizzes/client.ts
@@ -3,6 +3,11 @@ const axiosWithCredentials = axios.create({ withCredentials: true });
 const REMOTE_SERVER = import.meta.env.VITE_REMOTE_SERVER;
 const QUIZZES_API = `${REMOTE_SERVER}/api/quizzes`;
 
+export const findQuizById = async (quizId: string) => {
+    const response = await axiosWithCredentials.get(`${REMOTE_SERVER}/api/quizzes/${quizId}`);
+    return response;
+}
+
 export const createQuizForCourse = async (courseId: string, quiz: any) => {
     const response = await axiosWithCredentials.post(`${REMOTE_SERVER}/api/courses/${courseId}/quizzes`, quiz);
     return response.data;
@@ -22,3 +27,4 @@ export const deleteQuiz = async (quizId: string) => {
     const response = await axiosWithCredentials.delete(`${QUIZZES_API}/${quizId}`);
     return response.data;
 };
+
