352552491bc1af501b2621134ef3c8bf78d511a8
Ruby Mason <mason.r@northeastern.edu>
2025-06-21 22:48:38 -0400

    deploy

--- src/Kambaz/Courses/Quizzes/QuizDetails.tsx
+++ src/Kambaz/Courses/Quizzes/QuizDetails.tsx
@@ -6,7 +6,6 @@ import { useState, useEffect } from "react";
 import * as quizzesClient from "./client";
 import * as attemptsClient from "./Attempts/client";
 
-
 export default function QuizDetails() {
     const { cid, qid } = useParams();
     const navigate = useNavigate();
